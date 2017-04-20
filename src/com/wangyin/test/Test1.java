7c412504668df6b28ee20e35bc350244555d6871
FengXiaoXiaoXi <1126070636@qq.com>
2017-04-20 22:58:05 +0800

    test

--- src/com/wangyin/test/Test1.java
+++ src/com/wangyin/test/Test1.java
@@ -4,6 +4,12 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("我在 进行测试");
+		System.out.println("我在 进行测试");
+		System.out.println("我在 进行测试");
+		System.out.println("我在 进行测试");
+		System.out.println("我在 进行测试");
+		System.out.println("我在 进行测试");
+		System.out.println("我在 进行测试");
 	}
 
 }
