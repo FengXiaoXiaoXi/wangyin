6a1f8d5ff215d2a023bc85ac1a827fa5012e4507
FengXiaoXiaoXi <1126070636@qq.com>
2017-04-20 23:18:30 +0800

    oooooojojojo

--- src/com/wangyin/test1/WangYinTest.java
+++ src/com/wangyin/test1/WangYinTest.java
@@ -3,6 +3,28 @@ package com.wangyin.test1;
 public class WangYinTest {
 	public static void main(String[] args) {
 		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		System.out.println("北京你好");
+		
 		
 	}
 }
